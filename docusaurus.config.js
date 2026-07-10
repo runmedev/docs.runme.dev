a9e98b0ee0ddabb50638e58fc185bfb51c7c4419
Sebastian (Tiedtke) Huckleberry <sebastiantiedtke@gmail.com>
2026-07-10 16:44:40 -0700

    Update Runme eval announcement title (#707)
    Signed-off-by: Sebastian (Tiedtke) Huckleberry <sebastiantiedtke@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -219,7 +219,7 @@ const config = {
       announcementBar: {
         id: "runme_eval_agents_post",
         content:
-          '<a target="_blank" rel="noopener noreferrer" href="https://runme.dev/blog/runme-eval">Task evals for the other 99% of agent users &rarr;</a>',
+          '<a target="_blank" rel="noopener noreferrer" href="https://runme.dev/blog/runme-eval">New post: Sure, Your Claude Is Doing Amazing Things. Prove It. &rarr;</a>',
         backgroundColor: "#facc15",
         textColor: "#000000",
         isCloseable: true,
